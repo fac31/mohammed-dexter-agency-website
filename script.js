b135b3b759957fabf85278c4518f790899f67dcf
ShMuhammad <14.chaouni@gmail.com>
2024-03-30 01:06:19 +0000

    form validation

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const form = document.getElementById("form");
 const nameField = document.getElementById("nameField");
-const companyNameField = document.getElementById("companyNameField");
+const companyNameField = document.getElementById("CompanyNameField");
 const emailField = document.getElementById("emailField");
 const submitBtn = document.querySelector('input[type="submit"]');
-const validationSquare =  document.getElementById("square");
+const validationSquare =  document.querySelector(".square");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
+form.addEventListener('submit', function(event) {
+    // Prevent the form from submitting
+    event.preventDefault();
     checkInput();
+    
 });
 
 function checkInput(){
-    const nameValue = nameField.value.trim()
-    const companyValue = companyNameField.value.trim()
-    const emailValue = emailField.value.trim()
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(nameField.value.trim() === "" ) {
+        alert( "Please provide your name!" );
+        nameField.focus();
+        return false;
+    }
+
+    if(companyNameField.value.trim() === "" ) {
+        alert( "Please provide your name!" );
+        companyNameField.focus();
+        return false;
+    }
+
+
+
+     validationSquare.style.backgroundColor = "#82E79E";
+     alert('Form submitted successfully!');
 }
