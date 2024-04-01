7e1c3128fb6239b8c0a6578961da4d0f0dd1aa2f
ShMuhammad <14.chaouni@gmail.com>
2024-04-01 22:25:05 +0100

    Form updated

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const nameField = document.getElementById("nameField");
 const companyNameField = document.getElementById("CompanyNameField");
 const emailField = document.getElementById("emailField");
 const submitBtn = document.querySelector('input[type="submit"]');
-const validationSquare =  document.querySelector(".square");
 
 form.addEventListener('submit', function(event) {
     // Prevent the form from submitting
     event.preventDefault();
-    checkInput();
-    
+    checkInput(); 
 });
 
 function checkInput(){
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     if(nameField.value.trim() === "" ) {
         alert( "Please provide your name!" );
         nameField.focus();
@@ -21,13 +20,17 @@ function checkInput(){
     }
 
     if(companyNameField.value.trim() === "" ) {
-        alert( "Please provide your name!" );
+        alert( "Please provide the company name!" );
         companyNameField.focus();
         return false;
     }
 
+    // Validate email
+    if (!emailPattern.test(emailField.value.trim())) {
+        alert( "Please provide your email!" );
+        emailField.focus();
+        return false;
+    }
 
-
-     validationSquare.style.backgroundColor = "#82E79E";
-     alert('Form submitted successfully!');
+    alert('Form submitted successfully!');
 }
